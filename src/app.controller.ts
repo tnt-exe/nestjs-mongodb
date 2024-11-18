6d2c6e82576eec419c8a2257a6d772ae238b78a8
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-11-18 23:21:16 +0700

    add and test with swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiAcceptedResponse, ApiBody, ApiConsumes, ApiOkResponse, ApiProduces, ApiTags } from '@nestjs/swagger';
 
+@ApiTags("hmm2")
 @Controller()
+@ApiConsumes("application/json")
+@ApiProduces("application/json")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @ApiOkResponse({ description: "this is desc" })
+  @ApiBody({ type: String })
+  @Get("hello")
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Api example")
+    .setDescription("this is desc")
+    .setVersion("1.0")
+    .addTag("test tag")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
