b713e1306345e729067dd1c3aa7a181dc2d9b5e6
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-11-19 16:22:42 +0700

    add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './users/user.module';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1/nestjs-mongodb'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB),
     UserModule,
     PostsModule,
   ],
